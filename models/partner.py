c88e6b68ad0e31176b4de9d18f08e1927f782fe3
Hendra Nerita <hd.brandoz@gmail.com>
2016-11-01 13:38:36 +0700

    inheritance

---
+++ models/partner.py
@@ -0,0 +1,8 @@
+from odoo import fields, models
+
+class Partner(models.Model):
+	_inherit = 'res.partner'
+
+	# Add a new column to the res.partner model, by default partners are not instructors
+	instructor = fields.Boolean(string="Instructor", default=False)
+	session_ids = fields.Many2many('openacademy.session', string="Attended Sessions", readonly=True)
